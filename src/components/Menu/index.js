1f48c598b16ecebe124910fe1e0058a8c83f8604
Br4ida <gabrielbnaza@gmail.com>
2020-07-29 19:13:38 -0300

    Slider change

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Logo from '../../assets/img/Logo.png';
-import "./Menu.css";
+import './Menu.css';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
     return (
         <nav className="Menu">
             <Link to="/">
-                <img className="Logo" src={Logo} alt='Bagreflix logo' />
+                <img className="Logo" src={Logo} alt="Bagreflix logo" />
             </Link>
 
             <Button as={Link} className="ButtonLink" to="/cadastro/video">
--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Vídeo';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
+// Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :)
 const Pagina404 = () => (<div>Página 404</div>)
-
 
 ReactDOM.render(
     <BrowserRouter>
